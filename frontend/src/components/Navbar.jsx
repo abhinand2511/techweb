dd5b96f6cba1a2e6ab86d3a646373f66ac384ef4
abhinand2511 <abhinand25112001@gmail.com>
2025-08-01 00:07:43 -0700

    Navbar created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full z-10">
+      <div>
+        <img src={assets.logo} alt="" />
+        <ul>
+          <a
+            href="#Header"
+            className="text-gray-300 hover:text-white hover:bg-[#B0E57C] hover:px-4 hover:py-2 hover:rounded-full transition-all duration-300 ease-in-out"
+          >
+            Home
+          </a>
+          <a
+            href="#Header"
+            className="text-gray-300 hover:text-white hover:bg-[#B0E57C] hover:px-4 hover:py-2 hover:rounded-full transition-all duration-300 ease-in-out"
+          >
+            What We Do
+          </a>
+          <a
+            href="#Header"
+            className="text-gray-300 hover:text-white hover:bg-[#B0E57C] hover:px-4 hover:py-2 hover:rounded-full transition-all duration-300 ease-in-out"
+          >
+            About
+          </a>
+          <a
+            href="#Header"
+            className="text-gray-300 hover:text-white hover:bg-[#B0E57C] hover:px-4 hover:py-2 hover:rounded-full transition-all duration-300 ease-in-out"
+          >
+            Contact Us
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
