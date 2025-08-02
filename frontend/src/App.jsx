47fedd4bd63a81aad0ab67a137e0ce56c280fae9
abhinand2511 <abhinand25112001@gmail.com>
2025-08-02 05:49:46 -0700

    final push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import WhatWeDo from './components/WhatWeDo'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <WhatWeDo/>
+      <Footer/>
     </div>
   )
 }
